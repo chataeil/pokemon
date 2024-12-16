053db3752e287817a770e6c5434098642fa8e836
chataeil <ckxodlf12@naver.com>
2024-12-16 17:55:51 +0900

    이메일 인증
    code

--- src/test/java/org/koreait/email/EmailSendTest.java
+++ src/test/java/org/koreait/email/EmailSendTest.java
@@ -1,8 +1,9 @@
 package org.koreait.email;
 
-
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+import org.koreait.email.controllers.RequestEmail;
+import org.koreait.email.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,8 +12,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
-@ActiveProfiles({"default","test","email"})
+@ActiveProfiles({"default", "test", "email"})
 public class EmailSendTest {
     @Autowired
     private JavaMailSender javaMailSender;
@@ -20,28 +25,48 @@ public class EmailSendTest {
     @Autowired
     private SpringTemplateEngine templateEngine;
 
+    @Autowired
+    private EmailService service;
+
     @Test
-    void test1() throws Exception{
+    void test1() throws Exception {
         /**
-         * to : 받는 메일
+         * to : 받는 이메일
          * cc : 참조
          * bcc : 숨은 참조
-         * @throws Exception
          */
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
-        helper.setTo("ckxodlf12@gmail.com");
+        helper.setTo("yonggyo00@kakao.com");
         helper.setSubject("테스트 이메일 제목...");
         helper.setText("테스트 이메일 내용...");
         javaMailSender.send(message);
     }
+
     @Test
-    void test2(){
+    void test2() {
         Context context = new Context();
-        context.setVariable("subject", "테스트 제목 ...");
+        context.setVariable("subject", "테스트 제목...");
 
         String text = templateEngine.process("email/auth", context);
 
         System.out.println(text);
+    }
+
+    @Test
+    void test3() {
+        RequestEmail form = new RequestEmail();
+        form.setTo(List.of("ckxodlf12@naver.com", "ckxodlf12@naver.com"));
+        form.setCc(List.of("ckxodlf12@gmail.com"));
+        form.setBcc(List.of("ckxodlf12@gmail.com"));
+        form.setSubject("테스트 이메일 제목...");
+        form.setContent("<h1>테스트 이메일 내용...</h1>");
+
+        Map<String, Object> tplData = new HashMap<>();
+        tplData.put("key1", "값1");
+        tplData.put("key2", "값2");
+
+        boolean result = service.sendEmail(form, "auth", tplData);
+        System.out.println(result);
     }
 }
