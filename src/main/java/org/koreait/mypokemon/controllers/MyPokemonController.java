5611dfbec637f0335cfbcf4df0d5ae90c78f6d41
chataeil <ckxodlf12@naver.com>
2025-01-06 17:32:37 +0900

    마이포켓몬 수정
    코드

---
+++ src/main/java/org/koreait/mypokemon/controllers/MyPokemonController.java
@@ -0,0 +1,9 @@
+package org.koreait.mypokemon.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MyPokemonController {
+}
--- src/main/java/org/koreait/pokemon/repositories/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.koreait.pokemon.repositories;
-
-import org.koreait.pokemon.entities.Pokemon;
-import org.koreait.pokemon.entities.PokemonImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface ImageRepository extends JpaRepository<PokemonImage, Long>, QuerydslPredicateExecutor<Pokemon> {
-}
